47966f31cfaa7bb9ad46b8d1936b217092a33920
Aspe1ta <ausy11@live.ca>
2020-03-07 19:03:56 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -176,7 +176,7 @@ app.post("/addStudent", urlencodedParser, function(req, res) {
 
   let data = req.body;
 
-  data.attendanceRecord = [false, false, false, false, false, false, false, false, false, false];
+  data.attendanceRecord = [false, false, false, false, false, false, false, false, false, false, false, false];
 
   // console.log(data)
 
